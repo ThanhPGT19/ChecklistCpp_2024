654854d14f4e6f445d8cce6d1c78c8ea45e5bcd4
ThanhPGT19 <pgtthanh@gmail.com>
2024-03-25 21:35:36 +0700

    modifier linked_list library and add Makefile

--- Alogithm_DataStructure/Linked_list/inc/linked_list.cpp
+++ Alogithm_DataStructure/Linked_list/inc/linked_list.cpp
@@ -1,25 +1,65 @@
 #include "linked_list.h"
-#include <iostream>
-
-template<class T> class Node{
-    private:
-    T data;
-    T *pt_next;
-    public:
-    Node(T val){
-        this->data = val;
-        this->pt_next = NULL;
+template<class T> void Node <T> :: Add_element_Head(Node **ptr,T val){
+    Node *head = new Node(val);
+    //std:: cout<< head<<" SCOP"<<std::endl;
+    if(*ptr == NULL) *ptr = head;
+    else {
+        head->pt_next = *ptr;
+        *ptr = head;
     }
-    // Thêm phần tử vào đầu List
-    void Add_element_Head();
+}
+template <class T> void Node<T> :: Print_List(Node *head){
+    Node *pt = head;
+    int count =0;
+    while(pt!=NULL){
+        count ++;
+        std::cout<<pt->data<<std::endl;
+        pt = pt->pt_next;
+        if(count==20){
+            exit(1);
+        }
+    };
+}
     //Thêm phần tử vào cuối List
-    void Add_element_Tail();
+template<class T> void Node <T> :: Add_element_Tail(Node *head,T val){
+        Node *tail = new Node(val);
+        Node *pt = head;
+        while(pt->pt_next!=NULL){
+            pt=pt->pt_next;
+        }
+        pt->pt_next=tail;
+}
     //Thêm phần tử vào vitri bất kỳ List
-    void Add_element_
-
-};
-
+template<class T> void Node <T> :: Add_element_Pos(Node *head, int pos , T val){
+    Node *newelement = new Node(val);
+    Node *pt = head;
+    int count =0;
+    while(pt!=NULL&&count!=pos-1){
+        pt=pt->pt_next;
+        count++;
+    }
+    if(count!=pos-1){
+        return;
+    }
+    else{
+        newelement->pt_next = pt->pt_next;
+        pt->pt_next = newelement;
+    }
+}
+    //Xóa phần tử đầu List
+    template<class T> void Node <T> :: Del_element_Head(){}
+    //Xóa phần tử cuối List
+    template<class T> void Node <T> :: Del_element_Tail(){}
+    //Xóa phần tử vtri bất kỳ List
+    template<class T> void Node <T> :: Del_element_Pos(){}
 int main (){
-
+    Node<int>*head = new Node(5);
+    //std:: cout<< head<<std::endl;
+    head->Add_element_Head(&head,7);
+    head->Add_element_Head(&head,11);
+    head->Add_element_Head(&head,15);
+    head->Add_element_Pos(head,3,3030);
+    //std:: cout<< head<<std::endl;
+    head->Print_List(head);
     return 0;
 }
--- Alogithm_DataStructure/Linked_list/inc/linked_list.h
+++ Alogithm_DataStructure/Linked_list/inc/linked_list.h
@@ -1,6 +1,30 @@
 #if !defined LINKED_LIST_H__
 #define LINKED_LIST_H__
 #include <iostream>
+template<class T> class Node{
+    private:
+    T data;
+    Node *pt_next;
+    public:
+    Node(T val){
+        this->data = val;
+        this->pt_next = NULL;
+    }
+    //In List
+    void Print_List(Node *head);
+    // Thêm phần tử vào đầu List
+    void Add_element_Head(Node **ptr,T val);
+    //Thêm phần tử vào cuối List
+    void Add_element_Tail(Node *head,T val);
+    //Thêm phần tử vào vitri bất kỳ List
+    void Add_element_Pos(Node *head, int pos , T val);
+    //Xóa phần tử đầu List
+    void Del_element_Head();
+    //Xóa phần tử cuối List
+    void Del_element_Tail();
+    //Xóa phần tử vtri bất kỳ List
+    void Del_element_Pos();
 
+};
 
 #endif // LINKED_LIST_H__
