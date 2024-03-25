e5704e7bc0188d9cd6262932e0b8463e04427ed7
ThanhPGT19 <pgtthanh@gmail.com>
2024-03-25 10:56:03 +0700

    modifier vector

---
+++ Alogithm_DataStructure/Linked_list/inc/linked_list.cpp
@@ -0,0 +1,25 @@
+#include "linked_list.h"
+#include <iostream>
+
+template<class T> class Node{
+    private:
+    T data;
+    T *pt_next;
+    public:
+    Node(T val){
+        this->data = val;
+        this->pt_next = NULL;
+    }
+    // Thêm phần tử vào đầu List
+    void Add_element_Head();
+    //Thêm phần tử vào cuối List
+    void Add_element_Tail();
+    //Thêm phần tử vào vitri bất kỳ List
+    void Add_element_
+
+};
+
+int main (){
+
+    return 0;
+}
---
+++ Alogithm_DataStructure/Linked_list/inc/linked_list.h
@@ -0,0 +1,6 @@
+#if !defined LINKED_LIST_H__
+#define LINKED_LIST_H__
+#include <iostream>
+
+
+#endif // LINKED_LIST_H__
