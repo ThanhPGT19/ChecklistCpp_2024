07e823e24a47a4ec882ad3d1fca07e3f2664c1bc
ThanhPGT <pgtthanh99@gmail.com>
2024-03-28 17:33:13 +0700

    fix error Segmentation fault this->size =0 in constructor

--- Test/bss.cpp
+++ Test/bss.cpp
@@ -1,29 +1,9 @@
-#include<iostream>
-#include <vector>
-using namespace std;
-class  A
-{
-private:
-    int a;
-public:
-     A(int B):a(B){
-        cout<<"CONstructor"<<endl;
-     };
-    A(A const &obj){
-        cout<<"COPYY"<<endl;
-     }
-     A(A &&obj){
-        cout<<"MOVEEE"<<endl;
-     }
-    ~ A(){cout<<"Destructor "<<a<< endl;}
-};
 
 
-int main(){
-    vector <A> vec;
-    A obj1(9);
-    vec.push_back(A(5));
-    vec.push_back(obj1);
-    vec.push_back(A(1));
-return 0;
+const int x =7;
+
+char * a = "klsdjgfklsdfg" ;
+int main (){
+    //a[9]='5' ;
+    return 0;
 }
